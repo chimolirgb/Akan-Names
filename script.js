c2cb8379358a1661bc26dc999f323d34ab781620
chimolirgb <chimolilucy@gmail.com>
2021-04-09 22:58:56 +0300

    modified script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-var CC, YY, MM, DD, , modulus
+var CC, YY, MM, DD, modulus, modulusValue;
+let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sartuday"];
+let maleNames = [" Kwasi", "Kwadwo", "Kwabena", "Kwaku", " Yaw", "Kofi", "Kwame"];
+let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
